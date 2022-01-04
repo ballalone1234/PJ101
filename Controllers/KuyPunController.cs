9864b49884ac51404db7f5c9923b4942b8657b9e
ballalone1234 <ballpol.127@gmail.com>
2022-01-04 20:11:39 +0700

    Add project files.

---
+++ Controllers/KuyPunController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1.Controllers
+{
+    public class KuyPunController : Controller
+    {
+        // GET: KuyPun
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PPP()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public ActionResult Personnel()
+        {
+            return View();
+        }
+        public ActionResult Blacklist()
+        {
+            return View();
+        }
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        public ActionResult COURSE()
+        {
+            return View();
+        }
+        public ActionResult REPORT()
+        {
+            return View();
+        }
+        public ActionResult ACTIVATE()
+        {
+            return View();
+        }
+        public ActionResult TAG()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
